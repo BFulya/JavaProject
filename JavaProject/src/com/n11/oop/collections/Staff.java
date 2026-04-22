0b7351f465df4ab7d52032750ba7ec6b28dbe595
BasakFulya <basakfulyaytu@gmail.com>
2026-04-23 02:56:09 +0300

    2nd Course

---
+++ JavaProject/src/com/n11/oop/collections/Staff.java
@@ -0,0 +1,14 @@
+package com.n11.oop.collections;
+
+public class Staff {
+
+    String name;
+    String surname;
+    double salary;
+
+    public Staff(String name, String surname, double salary) {
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+}
---
+++ JavaProject/src/com/n11/oop/collections/Test.java
@@ -0,0 +1,40 @@
+package com.n11.oop.collections;
+
+import com.n11.oop.Human;
+
+import java.util.ArrayList;
+
+public class Test {
+
+    public static void main(String[] args)
+    {
+
+        ArrayList<String> list = new ArrayList<String>();
+        Human i = new Human();
+        list.add("Kevin");
+
+        if(list.contains(i))
+        {
+            System.out.println("exist in List ");
+        }
+        else
+        {
+
+        }
+
+
+        ArrayList<Staff> staffList = new ArrayList<Staff>();
+        Staff p1 = new Staff("john","doe",3000);
+        Staff p2 = new Staff("jane","done",4000);
+        staffList.add(p1);
+        staffList.add(p2);
+        staffList.add(new Staff("mary","lou",5000));
+        double sumOfSalary=0.0;
+        for(Staff p : staffList)
+        {
+            System.out.println("Staff's name : "+ p.name+ " surname : " +p.surname+ " salary : " +p.salary);
+            sumOfSalary+= p.salary;
+        }
+        System.out.println("Sum of salary : " +sumOfSalary);
+    }
+}
