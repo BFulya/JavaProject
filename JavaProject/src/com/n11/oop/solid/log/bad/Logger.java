7ffb09f305550d82837cc0fbd176cdac2a27196c
BasakFulya <basakfulyaytu@gmail.com>
2026-04-23 16:47:17 +0300

    update 2nd course

--- JavaProject/src/com/n11/oop/solid/log/bad/Logger.java
+++ JavaProject/src/com/n11/oop/solid/log/bad/Logger.java
@@ -2,4 +2,33 @@ package com.n11.oop.solid.log.bad;
 
 public class Logger {
 
+    private DbLog dbLog;
+    private XmlLog xmlLog;
+    private TextLog textLog;
+
+
+    public Logger(XmlLog xmlLog, DbLog dbLog,TextLog textLog)
+    {
+        this.xmlLog = xmlLog;
+        this.dbLog = dbLog;
+        this.textLog = textLog;
+
+    }
+
+    public void LogKayit(LogType type, String value)
+    {
+        switch(type)
+        {
+            case Xml:
+                xmlLog.xmlRecord(value);
+                break;
+            case Db:
+                dbLog.dbRecord(value);
+                break;
+            case Text:
+                textLog.textRecord(value);
+                break;
+        }
+    }
+
 }
