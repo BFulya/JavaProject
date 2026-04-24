2b205eaa7ac00c3b83ac57d0d1221337677847f0
BasakFulya <basakfulyaytu@gmail.com>
2026-04-25 00:59:41 +0300

    3rd course

--- aopornek/src/main/java/com/n11bootcamp/aopornek/AopornekApplication.java
+++ aopornek/src/main/java/com/n11bootcamp/aopornek/AopornekApplication.java
@@ -2,7 +2,9 @@ package com.n11bootcamp.aopornek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 @SpringBootApplication
 public class AopornekApplication {
 
